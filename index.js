313ddba283f2083e533c0fa24ab9b320f3e5b6f0
unknown <puth-rosita@lambdastudents.com>
2020-07-14 16:07:02 -0400

    added new file

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-
-
+let votingAge = 19; 
+if (votingAge > 18){
+console.log('true');
+}
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
+let firstName = 'John';
 
+ if( firstName === 'Dan'){
+
+  console.log('cool name');
+
+ }else {
+   console.log('not cool name');
+ }
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
+const birthYear = "1999";
 
+console.log(parseInt(birthYear)); 
 
 
 
 //Task d: Write a function to multiply a*b 
 
-
+function multiply(a,b) {
+  return a*b;
+}
+console.log(multiply(2,3));
 
 
 
